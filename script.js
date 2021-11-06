d9a947fe20a1e65bd7478812a66207dc5345ebe3
Karan Batavia <42770054+kbatavia12@users.noreply.github.com>
2021-11-06 13:09:44 +0530

    party clock

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-clock(); 
+clock();
 digitalTime();
 function clock() {
     const date = new Date();
@@ -10,7 +10,7 @@ function clock() {
     hours *= 30;
     minutes *= 6;
     seconds *= 6;
-    
+
     document.getElementById('clock__hour').style.transform = `rotate(${hours}deg)`;
     document.getElementById('clock__minute').style.transform = `rotate(${minutes}deg)`;
     document.getElementById('clock__second').style.transform = `rotate(${seconds}deg)`;
@@ -22,8 +22,8 @@ function digitalTime() {
     const date = new Date();
 
     let hours = date.getHours();
-    let minutes = date.getMinutes() < 10 ? '0' + date.getMinutes(): date.getMinutes();
-    let seconds = date.getSeconds() < 10 ? '0' + date.getSeconds(): date.getSeconds();
+    let minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+    let seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
 
     document.getElementById('digital__hour').innerText = `${hours}:`
     document.getElementById('digital__minutes').innerText = `${minutes}:`
@@ -32,5 +32,42 @@ function digitalTime() {
 
 }
 
+const digitalMain = document.getElementById('digital__main')
+const clockMain = document.getElementById('clock__container')
+const clockHour = document.getElementById('clock__hour')
+const clockMins = document.getElementById('clock__minute')
+const clockSeconds = document.getElementById('clock__second')
+const clockDot = document.getElementById('clock__dot')
+digitalMain.style = `cursor:pointer;`
 
-setInterval(() => {clock(); digitalTime()}, 1000);
+
+
+const randomRGB = () => {
+    const randomValueOne = Math.floor(Math.random() * 255);
+    const randomValueTwo = Math.floor(Math.random() * 255);
+    const randomValueThree = Math.floor(Math.random() * 255);
+
+
+    return `rgb(${randomValueOne}, ${randomValueTwo}, ${randomValueThree})`;
+}
+
+
+setInterval(() => {
+    clock(); digitalTime();
+
+
+
+    document.body.style.background = `linear-gradient(45deg, ${randomRGB()},${randomRGB()})`
+
+    digitalMain.style.color = `${randomRGB()}`;
+    clockMain.style.background = `${randomRGB()}`;
+    clockHour.style.background = `${randomRGB()}`;
+    clockMins.style.background = `${randomRGB()}`;
+    clockSeconds.style.background = `${randomRGB()}`;
+    clockDot.style.background = `${randomRGB()}`;
+
+
+
+
+
+}, 1000);
